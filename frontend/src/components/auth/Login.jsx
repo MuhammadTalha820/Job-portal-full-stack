2eb67c441577833259828c89cd56cf3eed66bbe6
Muhammad Talha <142047603+MuhammadTalha820@users.noreply.github.com>
2026-05-20 11:38:20 +0500

    Fix indentation and spacing in Login component

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -56,7 +56,7 @@ const Login = () => {
     return (
         <div>
             <Navbar />
-            <div className='flex items-center justify-center max-w-7xl mx-auto '>
+            <div className='flex items-center  justify-center max-w-7xl mx-auto '>
                 <form onSubmit={submitHandler} className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
                     <h1 className='font-bold text-xl mb-5'>Login</h1>
                     <div className='my-2'>
@@ -93,7 +93,7 @@ const Login = () => {
                                 />
                                 <Label htmlFor="r1">Student</Label>
                             </div>
-                            <div className="flex items-center space-x-2">
+                            <div className="flex items-center  space-x-2">
                                 <Input
                                     type="radio"
                                     name="role"
@@ -107,7 +107,7 @@ const Login = () => {
                         </RadioGroup>
                     </div>
                     {
-                        loading ? <Button className="w-full my-4"> <Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait </Button> : <Button type="submit" className="w-full my-4">Login</Button>
+                        loading ? <Button className="w-full  my-4"> <Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait </Button> : <Button type="submit" className="w-full my-4">Login</Button>
                     }
                     <span className='text-sm'>Don't have an account? <Link to="/signup" className='text-blue-600'>Signup</Link></span>
                 </form>
