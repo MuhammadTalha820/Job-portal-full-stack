97d18728e408e31341c65343ec092c3f78f53e17
Muhammad Talha <142047603+MuhammadTalha820@users.noreply.github.com>
2026-06-19 10:59:50 +0500

    Refactor Profile component layout and skills display

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -45,9 +45,9 @@ const Profile = () => {
                     </div>
                 </div>
                 <div className='my-5'>
-                    <h1>Skills</h1>
-                    <div className='flex items-center gap-1'>
-                        {
+                        <h1>Skills</h1>
+                        <div className='flex items-center gap-1'>
+                                {
                             user?.profile?.skills.length !== 0 ? user?.profile?.skills.map((item, index) => <Badge key={index}>{item}</Badge>) : <span>NA</span>
                         }
                     </div>
@@ -59,9 +59,9 @@ const Profile = () => {
                     }
                 </div>
             </div>
-            <div className='max-w-4xl mx-auto bg-white rounded-2xl'>
+                <div className='max-w-4xl mx-auto bg-white rounded-2xl' >
                 <h1 className='font-bold text-lg my-5'>Applied Jobs</h1>
-                {/* Applied Job Table   */}
+                    {/* Applied Job Table   */}
                 <AppliedJobTable />
             </div>
             <UpdateProfileDialog open={open} setOpen={setOpen}/>
